fdd7c0ac7dd6bc3b733f377a63579ed8c6eb6e97
ertank <tosss.beee@gmail.com>
2023-09-13 20:35:03 +0200

    fix typo

--- finetune_hf_llm.py
+++ finetune_hf_llm.py
@@ -602,7 +602,7 @@ def main():
 
 
     storage_path = (
-        f"/mnt_local_storage/ft_llms_with_deepspeed/{args.model_name}"
+        f"/mnt/local_storage/ft_llms_with_deepspeed/{args.model_name}"
     )
 
     trainer = TorchTrainer(
