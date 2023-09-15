1167d1fd55d2bd57f36d3823ea49b202765f17f6
ertank <tosss.beee@gmail.com>
2023-09-15 17:11:27 +0200

    change awsv2 to aws

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def get_hash_from_bucket(
 
     s3_sync_args = s3_sync_args or []
     subprocess.run(
-        ["awsv2", "s3", "cp", "--quiet"]
+        ["aws", "s3", "cp", "--quiet"]
         + s3_sync_args
         + [os.path.join(bucket_uri, "refs", "main"), "."]
     )
@@ -70,7 +70,7 @@ def download_model(
     path = get_download_path(model_id)
 
     cmd = (
-        ["awsv2", "s3", "sync"]
+        ["aws", "s3", "sync"]
         + s3_sync_args
         + (["--exclude", "*", "--include", "*token*"] if tokenizer_only else [])
         + [bucket_uri, path]
