b31c7dedc617e70ae9f53dce3b6e1f9f321d4028
ertank <tosss.beee@gmail.com>
2023-09-15 18:31:37 +0200

    change the source and tokens

--- create_dataset.py
+++ create_dataset.py
@@ -7,17 +7,15 @@ def main():
     if not os.path.exists("data"):
         os.mkdir("data")
 
-    print("Fetching the training data from s3://forensic-training-data/jsonl/")
-    os.system(f"aws s3 cp s3://forensic-training-data/jsonl/ training_data/ --recursive")
+    print("Fetching the training data from s3://forensic-training-data/ray/")
+    os.system(f"aws s3 cp s3://forensic-training-data/ray/ training_data/ --recursive")
     
 
     with open("data/tokens.json", "w") as f:
         tokens = {}
-        tokens["tokens"] = ["[INST]", "<<SYS>>", "<</SYS>>", "[/INST]"]
+        tokens["tokens"] = ["[INST]", "<<SYS>>", "<</SYS>>", "[/INST]", "<START_A>"]
         print('Adding special tokens to the vocabulary: ' + str(tokens["tokens"]))
         f.write(json.dumps(tokens))
-
-    print('[ACTION REQUIRED] Copy the training data from verita to ./data')
 
 
 if __name__ == "__main__":
